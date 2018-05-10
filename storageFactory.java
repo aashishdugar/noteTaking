a4eae5f5f03062e8a4673353b8b4fb533528ee13
Aashish Dugar <31813549+aashishdugar@users.noreply.github.com>
2018-05-10 16:39:18 -0400

    Update storageFactory.java

--- storageFactory.java
+++ storageFactory.java
@@ -13,26 +13,6 @@ function StorageFactory()
 }
 
 
-
-function StorageService() { /* ... */ }
-
-StorageService.prototype.createStorageContainer = function () { /* ... */ };
-
-StorageService.prototype.listStorageContainers = function () { /* ... */ };
-
-StorageService.prototype.createFolder = function () { /* ... */ };
-
-StorageService.prototype.deleteFolder = function () { /* ... */ };
-
-StorageService.prototype.createFile = function () { /* ... */ };
-
-StorageService.prototype.getFileByLocationId = function () { /* ... */ };
-
-module.exports = StorageService;
-
-
-
-
 // Constructor
 function AmazonStorageServiceImpl(accessKey, secretAccessKey) {
   
